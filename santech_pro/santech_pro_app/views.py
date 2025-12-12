abc627584cdf4225dfb8d8ffa428d8191f79eebf
Sanya <example@gmail.com>
2025-12-12 19:54:18 +0500

    z

--- santech_pro/santech_pro_app/views.py
+++ santech_pro/santech_pro_app/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def auf(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        print(email, password)
+    else:
+        return render(request, 'auf.html')
